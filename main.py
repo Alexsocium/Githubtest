f80b775e4673fc1c1c5f2b39488da184d814f432
Alexsocium <91365125+Alexsocium@users.noreply.github.com>
2021-09-27 10:30:04 +0300

    Добавил хендлер
    start

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from aiogram.dispatcher import Dispatcher
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message)
+    await message.reply('Hello /start')
